7af2d969252b095be946fc8c077c7bac0011ac89
mlizchap <mlizchap@gmail.com>
2019-02-17 01:48:44 -0500

    refactored creating a block div to its own function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    // const element = document.createElement('h1')
-    // element.innerHTML = "Hello World"
-    // document.body.appendChild(element)
-
     class Board {
       constructor() {
         this.blockArray = [12, 14, 11, 7, 6, 3, 4, 9, 1, 2, 5, 8, 10, 13, 15, 0];
       }
 
+      createBlock(val) {
+        const blockDiv= document.createElement("div")
+        blockDiv.innerHTML = val;
+        blockDiv.className = "block";
+        return blockDiv;
+      }
+
       createBoard() {
         this.blockArray.forEach(blockItem => {
-          
-          /* refactor to create block item */
-          const blockDiv= document.createElement("div")
-          blockDiv.innerHTML = blockItem;
-          blockDiv.className = "block";
-          
-          
-          document.getElementById("board").append(blockDiv);
+          const block = this.createBlock(blockItem)
+          document.getElementById("board").append(block);
       })
     }
   }
