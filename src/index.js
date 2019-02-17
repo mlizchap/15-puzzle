2a2794b278651841b54ac706a48bfbb12a4dbcf3
mlizchap <mlizchap@gmail.com>
2019-02-17 01:36:53 -0500

    created Board class and created a board out of an array of blocks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const element = document.createElement('h1')
-    element.innerHTML = "Hello World"
-    document.body.appendChild(element)
-  })
+    // const element = document.createElement('h1')
+    // element.innerHTML = "Hello World"
+    // document.body.appendChild(element)
+
+    class Board {
+      constructor() {
+        this.blockArray = [12, 14, 11, 7, 6, 3, 4, 9, 1, 2, 5, 8, 10, 13, 15, 0];
+      }
+
+      createBoard() {
+        this.blockArray.forEach(blockItem => {
+          
+          /* refactor to create block item */
+          const blockDiv= document.createElement("div")
+          blockDiv.innerHTML = blockItem;
+          blockDiv.className = "block";
+          
+          
+          document.getElementById("board").append(blockDiv);
+      })
+    }
+  }
+
+  const game = new Board();
+  game.createBoard();
+
+
+})
