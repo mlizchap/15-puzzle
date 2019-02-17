dc7b3250477c357e7debda48f2cf091c3ef797b1
mlizchap <mlizchap@gmail.com>
2019-02-17 02:12:10 -0500

    added function that determins if the user selected block is near the empty space.  If it is the selected block is swapped out with the empty space

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     class Board {
       constructor() {
-        this.selectBlock = null;
+        this.selectedBlock = null;
         this.blockArray = [12, 14, 11, 7, 6, 3, 4, 9, 1, 2, 5, 8, 10, 13, 15, 0];
       }
 
@@ -9,11 +9,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
         const blockDiv= document.createElement("div")
         blockDiv.innerHTML = val;
         blockDiv.className = "block";
-        blockDiv.addEventListener("click", () => this.selectBlock(blockDiv));
+        blockDiv.addEventListener("click", () => this.selectBlock(val));
         return blockDiv;
       }
 
-      createBoard() {
+      clearBoard() {
+        let element = document.getElementById("board");
+        while (element.firstChild) {
+          element.removeChild(element.firstChild);
+        }
+      }
+
+      renderBoard() {
+        // if (this.selectedBlock) {
+          this.clearBoard();
+        //}
         this.blockArray.forEach(blockItem => {
           const block = this.createBlock(blockItem)
           document.getElementById("board").append(block);
@@ -22,12 +32,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
       selectBlock(blockDiv) {
         this.selectedBlock = blockDiv;
+        console.log(blockDiv)
         this.reOrderBlocks();
+      }
+
+      reOrderBlocks() {
+        const selectedIndex = this.blockArray.indexOf(this.selectedBlock)
+        const emptyIndex = this.blockArray.indexOf(0);
+        
+        const itemIsToRightOfEmptySlot = selectedIndex + 1 === emptyIndex;
+        const itemIsToLeftOfEmptySlot = selectedIndex - 1 === emptyIndex;
+        const itemIsAboveEmptySlot = selectedIndex + 4 === emptyIndex;
+        const itemIsBelowEmptyItem = selectedIndex - 4 === emptyIndex;
+        
+        if (itemIsToRightOfEmptySlot || itemIsToLeftOfEmptySlot || itemIsAboveEmptySlot || itemIsBelowEmptyItem) {
+          [this.blockArray[selectedIndex], this.blockArray[emptyIndex]] = [this.blockArray[emptyIndex], this.blockArray[selectedIndex]]
+        }
+        
+        this.renderBoard();
       }
   }
 
   const game = new Board();
-  game.createBoard();
+  game.renderBoard();
 
 
 })
